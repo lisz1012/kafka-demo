b7093a95b64f5ef5858a2bb2c31abebb17cf10a8
lisz1012 <lisz1012@163.com>
2021-06-20 21:17:01 -0700

    checkstyle.xml

---
+++ src/main/java/com/lisz/kafka/Person.java
@@ -0,0 +1,30 @@
+package com.lisz.kafka;
+
+public class Person {
+	volatile int age = 10;
+
+	public void m() {
+		age = 100;
+		age ++;
+	}
+
+}
+
+/*
+ 0 aload_0
+ 1 invokespecial #1 <java/lang/Object.<init>>
+ 4 aload_0
+ 5 bipush 10
+ 7 putfield #2 <com/lisz/kafka/Person.age>
+10 return
+ */
+
+
+/*
+ 0 aload_0
+ 1 invokespecial #1 <java/lang/Object.<init>>
+ 4 aload_0
+ 5 bipush 10
+ 7 putfield #2 <com/lisz/kafka/Person.age>
+10 return
+ */
